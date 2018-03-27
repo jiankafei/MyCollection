6e80dd56ad2d4db7b9b95f0d97fd45257752d7de
zr <Wll001992Yx@163.com>
2018-03-27 10:13:53 +0800

    add qrcode

---
+++ Imitate/qrcode/QR8bitByte.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import QRConst from './QRConst';
+
+const QRMode = QRConst.QRMode;
+
+class QR8bitByte {
+	constructor(data){
+		this.mode = QRMode.MODE_8BIT_BYTE;
+		this.data = data;
+		this.parsedData = [];
+		// Added to support UTF-8 Characters
+		for (let i = 0, l = this.data.length; i < l; i++) {
+			const byteArray = [];
+			let code = this.data.charCodeAt(i);
+
+			if (code > 0x10000) {
+				byteArray[0] = 0xF0 | ((code & 0x1C0000) >>> 18);
+				byteArray[1] = 0x80 | ((code & 0x3F000) >>> 12);
+				byteArray[2] = 0x80 | ((code & 0xFC0) >>> 6);
+				byteArray[3] = 0x80 | (code & 0x3F);
+			} else if (code > 0x800) {
+				byteArray[0] = 0xE0 | ((code & 0xF000) >>> 12);
+				byteArray[1] = 0x80 | ((code & 0xFC0) >>> 6);
+				byteArray[2] = 0x80 | (code & 0x3F);
+			} else if (code > 0x80) {
+				byteArray[0] = 0xC0 | ((code & 0x7C0) >>> 6);
+				byteArray[1] = 0x80 | (code & 0x3F);
+			} else {
+				byteArray[0] = code;
+			}
+
+			this.parsedData.push(byteArray);
+		}
+		this.parsedData = Array.prototype.concat.apply([], this.parsedData);
+		if (this.parsedData.length !== this.data.length) {
+			this.parsedData.unshift(191);
+			this.parsedData.unshift(187);
+			this.parsedData.unshift(239);
+		}
+	}
+	getLength(buffer){
+		return this.parsedData.length;
+	}
+	write(buffer){
+		const parsedData = this.parsedData;
+		for (const data of parsedData) {
+			buffer.put(data, 8);
+		}
+	}
+}
+
+export default QR8bitByte;
---
+++ Imitate/qrcode/QRCode.js
@@ -0,0 +1,161 @@
+'use strict';
+/**
+ * L (Low)	~7%
+ * M (Medium)	~15%
+ * Q (Quartile)	~25%
+ * H (High)	~30%
+ */
+/**
+const qr = new QrCode({
+	size: 256, // 二维码大小，默认256，size是期望宽度，真实宽度以实例属性width为准
+	typeNumber: -1, // 二维码的计算模式，[1, 40] 默认-1为自动检测
+	correctLevel: 'H', // 二维码纠错级别 [L, M, Q, H] 默认H
+	colorDark: '#000000', // 前景颜色，默认黑色
+	colorLight: '#ffffff', // 背景颜色，默认白色
+	borderWidth: 0, // 边框宽度，int
+	borderColor: '#ffffff', // 边框颜色
+	text: '', // 文本信息，必填
+	ctx: , // context对象，必填
+});
+属性：
+	qr.width // 二维码宽度
+	qr.totalWidth // 二维码总体宽度，包括边框
+	qr.ctx // canvas的 context 对象
+	qr.typeNumber // 类型值
+方法：
+	qr.getCount(text, typeNumber = -1, correctLevel = 'H');
+	qr.isDark(row, col); // 在外部循环count时使用来判断颜色深浅
+	qr.renderToCanvas(x = 0, y = 0); // 绘制到canvas，默认[0, 0]，包括边框
+	qr.clear(startX, startY, width, height) // 清除某区域绘制
+
+注意：
+	1. 当初始化实例时，不传options，则可以通过getCount()获取数量以及使用isDark()来判断颜色深浅，从而自定义绘制；
+	2. 自定义绘制下，renderToCanvas() 无法使用；
+	3. 可以使用 clear() 方法清除区域绘制；
+	3. 一般情况下，直接初始化实例时传入参数，使用 renderToCanvas() 调用即可，clear在内部已经调用；
+ */
+import QRCodeModel from './QRCodeModel';
+import QRConst from './QRConst';
+const QRErrorCorrectLevel = QRConst.QRErrorCorrectLevel;
+const QRCodeLimitLength = QRConst.QRCodeLimitLength;
+
+const getUTF8Length = function (text) {
+	let replacedText = encodeURI(text).toString().replace(/\%[0-9a-fA-F]{2}/g, 'a');
+	return replacedText.length + (replacedText.length !== text.length ? 3 : 0);
+};
+
+const getTypeNumber = function (text, correctLevel) {
+	let type = 1;
+	let length = getUTF8Length(text);
+
+	for (let i = 0, l = QRCodeLimitLength.length; i <= l; i++) {
+		let limit = 0;
+		const limitLength = QRCodeLimitLength[i];
+		switch (correctLevel) {
+			case QRErrorCorrectLevel.L:
+				limit = limitLength[0];
+				break;
+			case QRErrorCorrectLevel.M:
+				limit = limitLength[1];
+				break;
+			case QRErrorCorrectLevel.Q:
+				limit = limitLength[2];
+				break;
+			case QRErrorCorrectLevel.H:
+				limit = limitLength[3];
+				break;
+		}
+
+		if (length <= limit) {
+			break;
+		} else {
+			type++;
+		}
+	}
+	if (type > QRCodeLimitLength.length) {
+		throw new Error('Too long data');
+	}
+	// console.log(`typeNumber: ${type}`);
+	return type;
+};
+
+class QRCode {
+	constructor(options) {
+		this.diy = false;
+		if (!options) {
+			this.diy = true;
+			return;
+		}; // 用于自定义绘制
+		if (!options.ctx) {
+			console.warn('please set ctx option!');
+			return;
+		}
+		if (!options.text) {
+			console.warn('please set text option!');
+			return;
+		}
+
+		const opts = Object.assign(this, {
+			size: 256,
+			typeNumber: -1,
+			colorDark: '#000000',
+			colorLight: '#ffffff',
+			correctLevel: 'H',
+			borderWidth: 0,
+			borderColor: '#ffffff',
+		}, options);
+
+		this.borderWidth = ~~this.borderWidth;
+
+		this.count = this.getCount(this.text, this.typeNumber, this.correctLevel);
+		this.tile = Math.round(this.size / this.count);
+		this.width = this.count * this.tile;
+	}
+	// 返回 count
+	getCount(text, typeNumber = -1, correctLevel = 'H'){
+		this.correctLevel = correctLevel;
+		this.typeNumber = typeNumber === -1 ? getTypeNumber(text, QRErrorCorrectLevel[correctLevel]) : typeNumber;
+		this._QRCodeModel = new QRCodeModel(this.typeNumber, QRErrorCorrectLevel[correctLevel]);
+		this._QRCodeModel.addData(text);
+		this._QRCodeModel.make();
+		const count = this._QRCodeModel.getModuleCount();
+		return count;
+	}
+	isDark(row, col){
+		return this._QRCodeModel.isDark(row, col);
+	}
+	// 绘制
+	renderToCanvas(x = 0, y = 0){
+		if (this.diy) return;
+		const {colorDark, colorLight, count, tile, width, borderWidth, borderColor, ctx} = this;
+		const totalWidth = this.totalWidth =  borderWidth * 2 + width;
+		this.startX = x;
+		this.startY = y;
+		// 先清后画
+		this.clear(x, y, totalWidth, totalWidth);
+
+		// 绘制边框
+		ctx.setFillStyle(borderColor);
+		ctx.fillRect(x, y, totalWidth, borderWidth);
+		ctx.fillRect(x + width + borderWidth, y, borderWidth, totalWidth);
+		ctx.fillRect(x, y + width + borderWidth, totalWidth, borderWidth);
+		ctx.fillRect(x, y, borderWidth, totalWidth);
+
+		// 绘制二维码
+		for (let row = 0; row < count; row++) {
+			for (let col = 0; col < count; col++) {
+				const isDark = this.isDark(row, col);
+				const tileX = x + borderWidth + col * tile;
+        		const tileY = y + borderWidth + row * tile;
+				ctx.fillStyle = isDark ? colorDark : colorLight;
+				ctx.fillRect(tileX, tileY, tile, tile);
+			}
+		}
+	}
+	// 清除
+	clear(startX, startY, width, height) {
+		this.ctx.clearRect(startX, startY, width, height);
+	}
+};
+
+export default QRCode;
